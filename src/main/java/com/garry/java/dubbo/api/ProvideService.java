2bfdab21d2263a9e514be1816bef63cbdf26b18b
garry <yijihua@foxmail.com>
2017-09-30 15:08:46 +0800

    dubbo服务接口定义

---
+++ src/main/java/com/garry/java/dubbo/api/ProvideService.java
@@ -0,0 +1,17 @@
+/**
+ * 深圳金融电子结算中心
+ * Copyright (c) 1995-2017 All Rights Reserved.
+ */
+package com.garry.java.dubbo.api;
+
+/**
+ * 
+ * @author Garry
+ * @version $Id: ProvideService.java, v 0.1 2017年9月30日 下午2:03:46 Garry Exp $
+ */
+public interface ProvideService {
+    /**
+     * 获取提供方信息
+     */
+    String getProvideInfo();
+}
